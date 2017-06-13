1366a67e905d5516d62d338824b2dc381483ce09
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-12 22:57:32 -0700

    centered subscribe bar

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@
 
 $(function () {
   
+  //tabindex bug fix
+  $("a[href^='#']").not("a[href='#']").click(function() {
+      $("#"+$(this).attr("href").slice(1)+"").focus();
+   });
+
+  //  flickity
 $('.container-products-list').flickity({
   cellAlign: 'left',
   wrapAround: true,
